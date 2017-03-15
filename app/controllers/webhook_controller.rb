fdc32998dce2bf3c9de549ed6240655c412defae
yuta <yuta@yutas-MacBook-Air.local>
2017-03-15 23:19:36 +0900

    fix webhool controller

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -12,34 +12,12 @@ class WebhookController < ApplicationController
     render :nothing => true, status: :ok
   end
 
-  def reminder
-    reminders = get_reminders
-    logger.info(reminders)
-
-    reminders.each do |reminder|
-      res = line_client.push(reminder)
-      logger.info(res.body)
-    end
-  end
-
   private
   def reply(params)
     response_service = response_service(params)
     response_text, reply_token  = response_service.form_response
 
     line_client.reply(reply_token, response_text)  if response_text.present?
-  end
-
-  def get_reminders
-    reminder_service.execute
-  end
-
-  def response_service(input_text)
-    ResponseService.new(input_text)
-  end
-
-  def reminder_service
-    ReminderService.new
   end
 
   def line_client
